5e47be0d3a36e723cfc151a9fa33f2a57a1c50c3
CruzMayra <velazquez.leonora@gmail.com>
2018-05-17 20:41:47 -0500

    creación de slide con jQuery

---
+++ js/app.js
@@ -0,0 +1,28 @@
+var target = 0;
+console.log($('li.active'))
+
+$(document).ready(function(){
+  var $buttons = $(".control");
+  $buttons.click(uploadImage);
+})
+
+var uploadImage = function() {
+  var $target = $(this).data("target");
+  showImage($target);
+  changeColorButton($target);
+}
+
+var showImage = function(target) {
+  var $currentSlide = $("li.active");
+  var $newImage = $("li[data-slide=" + target + "]");
+  console.log($currentSlide,$newImage);
+  $currentSlide.removeClass("active");
+  $newImage.addClass("active");
+}
+
+var changeColorButton = function(target) {
+  var $currentButton = $("button.active");
+  var $newButton = $("button[data-target=" + target + "]");
+  $currentButton.removeClass("active");
+  $newButton.addClass("active");
+}
